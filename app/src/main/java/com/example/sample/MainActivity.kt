4848c32397030939c187ace280e0b9588aaf9b69
yuya-yuya <yytyfp5115@icloud.com>
2021-02-10 10:55:25 +0900

    additionalcommit

--- app/src/main/java/com/example/sample/MainActivity.kt
+++ app/src/main/java/com/example/sample/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
 
@@ -22,5 +23,24 @@ class MainActivity : AppCompatActivity() {
             textView.text = "ナミ"
             imageView.setImageResource(R.drawable.nami)
         }
+    }
+
+    fun tapButton(view: View) {
+
+        var name = editText.text.toString()
+
+        if(name == "luffy"){
+            textView.text = "ルフィ"
+            imageView.setImageResource(R.drawable.luffy)
+        }else if(name == "zoro"){
+            textView.text = "ゾロ"
+            imageView.setImageResource(R.drawable.zoro)
+        }else if(name == "nami"){
+            textView.text = "ナミ"
+            imageView.setImageResource(R.drawable.nami)
+        }else{
+            textView.text = "そんなキャラはいません"
+        }
+
     }
 }
